c092afe11f79999102681047f3f3a8baa35bdcdb
chickey96 <c.m.hickey06@gmail.com>
2020-03-01 21:19:06 -0800

    Add user edit react component

--- frontend/components/users/user_edit.jsx
+++ frontend/components/users/user_edit.jsx
@@ -14,14 +14,14 @@ class UserEdit extends React.Component {
 
 
     return (
-      <div className="user-edit-page">
+      <div id="user-edit-page">
         <div id="user-edit-navigation-box">
           <div><button>Public Profile</button></div>
           <div><button>Settings</button></div>
           <div><button>Sign Out</button></div>
         </div>
-        Your Public Profile
-        <div>
+
+        <div id="user-edit-main-content">
           <input placeholder={this.props.currentUser.username}></input>
           <input placeholder={this.props.currentUser.email}></input>
           <input placeholder={this.props.currentUser.username}></input>
