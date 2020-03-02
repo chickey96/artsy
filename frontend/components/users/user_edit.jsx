f2df4c185647837e13309e6991b7686265a5373b
chickey96 <c.m.hickey06@gmail.com>
2020-03-02 00:44:27 -0800

    Finish frontend for user edit

--- frontend/components/users/user_edit.jsx
+++ frontend/components/users/user_edit.jsx
@@ -15,16 +15,55 @@ class UserEdit extends React.Component {
 
     return (
       <div id="user-edit-page">
-        <div id="user-edit-navigation-box">
-          <div><button>Public Profile</button></div>
-          <div><button>Settings</button></div>
-          <div><button>Sign Out</button></div>
+        <div className="user-edit-category" id="user-edit-username">
+          <h2>Username</h2>
+          <div className="content-to-edit">
+            <div className="font-11">Current Username</div>
+            <div className="font-11">fake-username</div>
+            <div className="section-divider"></div>
+            <div className="font-14 bold">Change your username</div>
+          </div>
+          <form action="">
+            <label className="user-edit-label">New Username</label>
+            <input className="user-edit-input" type="text"/>
+            <label className="user-edit-label">Confirm New Username</label>
+            <input className="user-edit-input" type="text"/>
+            <label className="user-edit-label">Your Etsy Password</label>
+            <input className="user-edit-input" type="text"/>
+            <button className="user-edit-submit">Change Username</button>
+          </form>
         </div>
 
-        <div id="user-edit-main-content">
-          <input placeholder={this.props.currentUser.username}></input>
-          <input placeholder={this.props.currentUser.email}></input>
-          <input placeholder={this.props.currentUser.username}></input>
+        <div className="user-edit-category" id="user-edit-password">
+          <h2>Password</h2>
+          <form action="">
+            <label className="user-edit-label">Current Password</label>
+            <input className="user-edit-input" type="text"/>
+            <label className="user-edit-label">New Password</label>
+            <input className="user-edit-input" type="text"/>
+            <label className="user-edit-label">Confirm New Password</label>
+            <input className="user-edit-input" type="text"/>
+            <button className="user-edit-submit">Change Password</button>
+          </form>
+        </div>
+
+        <div className="user-edit-category" id="user-edit-email">
+          <h2>Email</h2>
+          <div className="content-to-edit">
+            <div className="font-11">Current Email</div>
+            <div className="font-11">email@email.com</div>
+            <div className="section-divider"></div>
+            <div className="font-14 bold">Change your email</div>
+          </div>
+          <form action="">
+            <label className="user-edit-label">New Email</label>
+            <input className="user-edit-input" type="text"/>
+            <label className="user-edit-label">Confirm New Email</label>
+            <input className="user-edit-input" type="text"/>
+            <label className="user-edit-label">Your Etsy Password</label>
+            <input className="user-edit-input" type="text"/>
+            <button className="user-edit-submit">Change Email</button>
+          </form>
         </div>
       </div>
     )};
