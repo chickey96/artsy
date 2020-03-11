f585ff56b2b6ece6a55e8a5415c58e9144a0e4d9
chickey96 <c.m.hickey06@gmail.com>
2020-03-11 07:03:07 -0700

    Work on general styling including mobile

--- frontend/components/product/product_show.jsx
+++ frontend/components/product/product_show.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import CommentIndexContainer from './../comments/comment_index_container';
 
+const geography_helper = require('countrycitystatejson');
+const us_states = geography_helper.getStatesByShort('US');
 class ProductShow extends React.Component {
 
   constructor(props){
@@ -43,6 +45,28 @@ class ProductShow extends React.Component {
     return false;
   }
 
+  getRandomNumber(length){
+    return Math.floor(Math.random() * length);
+  }
+
+  randomTime(){
+    const first_num = this.getRandomNumber(5) + 1;
+
+    return `${first_num}-${first_num + 1} business days`;
+  }
+
+  randomCity(){
+    const random_state_idx = this.getRandomNumber(us_states.length);
+    const random_state = us_states[random_state_idx];
+    const cities = geography_helper.getCities('US', random_state);
+
+    if (cities.length == 0) return `${random_state}`;
+
+    const random_city_idx = this.getRandomNumber(cities.length);
+
+    return `${cities[random_city_idx]}, ${random_state}`;
+  }
+
   render () {
     if(!this.props.product) return null;
 
@@ -73,38 +97,55 @@ class ProductShow extends React.Component {
             <div className="product-section">
 
               <div className="show-image">
-                <div className="image-placeholder">
-                  <img src={this.props.product.photoUrl}
-                       className="image-show" />
-                </div>
+                <img src={this.props.product.photoUrl} className="image-show" />
               </div>
 
-              <div className="show-info">
-                <div className="show-artist"> {this.props.product.artist} </div>
-                <div className="show-title"> {this.props.product.title} </div>
-                <div className="show-price"> ${this.props.product.price}.00 </div>
+              <div id="show-info">
+                <div id="show-artist"> {this.props.product.artist} </div>
+                <div id="show-title"> {this.props.product.title} </div>
+                <div id="show-price"> ${this.props.product.price}.00 </div>
                 {cartOption}
 
-                <div className="product-description">
+                <div id="product-description">
 
-                  <div className="general-product-data">
-                    <div> &#x1f590; </div>
-                    <p> Handmade </p>
+                  <div id="general-specs" className="flex-container vertical">
+                    <div className="flex-container horizontal">
+                      <div className="product-data-icon"> &#x1f590; </div>
+                      <p> Handmade </p>
+                    </div>
+
+                    <div className="flex-container horizontal">
+                      <div className="product-data-icon"> &#x1f3f7; </div>
+                      <p>Made to order</p>
+                    </div>
                   </div>
 
-                  <div className="general-product-data" id="made-to-order">
-                    <div> &#x1f3f7; </div>
-                    <p>Made to order</p>
+                  <div className="flex-container vertical">
+                    <p className="title"> Material </p>
+                    <div className="content"> {materials} </div>
                   </div>
 
-                  <div className="product-materials">
-                    <p> Material </p>
-                    <div> {materials} </div>
+                  <div className="flex-container vertical">
+                    <div className="flex-container vertical">
+                      <p className="title"> Ready to ship in </p>
+                      <div className="content"> {this.randomTime()} </div>
+                    </div>
+
+                    <div className="flex-container vertical">
+                      <p className="title"> From </p>
+                      <div className="content"> {this.randomCity()} </div>
+                    </div>
+
+                    <div className="flex-container vertical">
+                      <p className="title"> Cost to ship </p>
+                      <div className="content"> Free </div>
+                    </div>
                   </div>
                 </div>
 
               </div>
             </div>
+
           <div className="comment-index">
             <div className="major-line"></div>
             <div className="comment-header">Comments</div>
