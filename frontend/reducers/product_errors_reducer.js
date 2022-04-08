28ebf5c0fff9f4a6415c4bb1b10f2f049c6a7e89
corinne <c.m.hickey06@gmail.com>
2022-04-08 11:06:53 -0700

    Import type constants, define reducer and freeze state

--- frontend/reducers/product_errors_reducer.js
+++ frontend/reducers/product_errors_reducer.js
@@ -1,2 +1,6 @@
+import { RECEIVE_PRODUCT_ERRORS,
+         RECEIVE_PRODUCT } from "../actions/product_action";
 
-         
+const productErrorsReducer = (state = [], action) => {
+    Object.freeze(state);
+}
