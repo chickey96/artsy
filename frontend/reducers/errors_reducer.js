5ebbad1239965fa90cc9c7a807af5afca39a11bd
corinne <c.m.hickey06@gmail.com>
2022-04-08 11:19:30 -0700

    Combine all error reducers to structure product errors data nested under errors in state

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import commentErrorsReducer from './comment_errors_reducer';
+import productErrorsReducer from './product_errors_reducer';
 
 export default combineReducers({
   session: sessionErrorsReducer,
-  comments: commentErrorsReducer
+  comments: commentErrorsReducer,
+  products: productErrorsReducer,
 });
