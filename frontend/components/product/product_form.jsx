b9af344989f30258e5cbc64c934a5a9213011778
corinne <c.m.hickey06@gmail.com>
2022-04-08 11:52:36 -0700

    Return a basic form in render function for users to create new product listings

--- frontend/components/product/product_form.jsx
+++ frontend/components/product/product_form.jsx
@@ -5,6 +5,34 @@ class ProductForm extends React.Component {
     constructor(props){
         super(props);
     }
+
+    render () {
+        return (
+            <form>
+                <h1>Add a New Listing</h1>
+                <h2>Photos</h2>
+
+                <h2>Listing details</h2>
+                <label> Title
+                    <input type="text" />
+                </label>
+
+                <label> Category 
+                    <input type="text" />
+                </label>
+
+                <label> Materials 
+                    <input type="text" />
+                </label>
+
+                <h2>Inventory and Pricing</h2>
+
+                <label> Price 
+                    <input type="text" />
+                </label>
+            </form>
+        )
+    }
     
 }
 
