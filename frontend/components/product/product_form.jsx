e1e9ec270a2199939f9dd460bccc01c07c8c6b93
corinne <c.m.hickey06@gmail.com>
2022-04-15 19:53:56 -0700

    Update form fields on change and create product on save

--- frontend/components/product/product_form.jsx
+++ frontend/components/product/product_form.jsx
@@ -4,6 +4,28 @@ class ProductForm extends React.Component {
 
     constructor(props){
         super(props);
+        this.save = this.save.bind(this)
+        this.state = { title: '', category: '', materials:'', price: ''}
+        this.filterErrors = this.filterErrors.bind(this)
+    }
+
+    update(field){
+        return e => this.setState({ [field]: e.target.value })
+    }
+
+    save(){
+        let product = { 
+            title: this.state.title, 
+            price: this.state.price, 
+            materials: `${this.state.category}:${this.state.materials}`,
+            artist_id: this.props.currentUser.id
+        }
+
+        this.createProduct(product)
+    }
+
+    filterErrors(keywords){
+
     }
 
     render () {
@@ -19,12 +41,14 @@ class ProductForm extends React.Component {
                         <div className="subheading">Listing details</div>
                         <label className="product-form-item"> 
                             <div>Title</div>
-                            <input type="text" />
+                            <input type="text" 
+                                   value={this.state.title}
+                                   onChange={this.update('title')} />
                         </label>
 
                         <label className="product-form-item">  
                             <div>Category</div>
-                            <select>                        
+                            <select onChange={this.update('category')}>                        
                                 <option value="mixed-media">mixed-media</option>
                                 <option value="drawing">drawing</option>
                                 <option value="painting">painting</option>
@@ -34,7 +58,9 @@ class ProductForm extends React.Component {
 
                         <label className="product-form-item"> 
                             <div>Materials</div>
-                            <input type="text" />
+                            <input type="text" 
+                                   value={this.state.title}
+                                   onChange={this.update('materials')}/>
                         </label>
                     </div>
 
@@ -42,10 +68,14 @@ class ProductForm extends React.Component {
                         <div className="subheading">Pricing</div>
                         <label className="product-form-item"> 
                             <div>Price</div> 
-                            <input type="text" />
+                            <input type="text" 
+                                   value={this.state.title}
+                                   onChange={this.update('price')}/>
                         </label>
                     </div>
-                    <button className="button black small">Save</button>
+                    <button onClick={this.save()} className="button black small">
+                        Save
+                    </button>
                 </form>
             </div>
         )
