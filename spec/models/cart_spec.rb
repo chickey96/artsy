49de30c16d1561cc13a216f580807cec9cca9ce0
corinne <c.m.hickey06@gmail.com>
2022-05-17 07:59:10 -0700

    Write unit tests for cart model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:product_id) }
+  end
+
+  describe "associations" do 
+    it { should belong_to(:user) }
+    it { should belong_to(:product) }
+  end
 end
